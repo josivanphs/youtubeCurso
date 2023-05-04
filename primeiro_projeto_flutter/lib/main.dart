553942b07b3a96926cb404cc34568da35fef2a88
Josivan da S. Marques <72993762+josivanphs@users.noreply.github.com>
2023-05-04 10:46:52 -0400

    first commit

---
+++ primeiro_projeto_flutter/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Mygae extends StatelessWidget {
+  const Mygae({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        color: Colors.white,
+      ),
+    );
+  }
+}
