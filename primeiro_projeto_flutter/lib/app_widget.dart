a122b7413f6096943e60598cc6140ac0fdd660f5
Josivan da S. Marques <72993762+josivanphs@users.noreply.github.com>
2023-05-05 14:14:25 -0400

    text - aula 17

--- primeiro_projeto_flutter/lib/app_widget.dart
+++ primeiro_projeto_flutter/lib/app_widget.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:primeiro_projeto_flutter/app_controller.dart';
+import 'package:primeiro_projeto_flutter/login_page.dart';
 
+// ignore: unused_import
 import 'home_page.dart';
 
 class AppWidget extends StatelessWidget {
@@ -7,9 +10,19 @@ class AppWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.red),
-      home: const HomePage(),
+    return AnimatedBuilder(
+      animation: AppController.instance,
+      builder: (context, child) {
+        return MaterialApp(
+          theme: ThemeData(
+            primarySwatch: Colors.red,
+            brightness: AppController.instance.isDartTheme
+                ? Brightness.dark
+                : Brightness.light,
+          ),
+          home: const LoginPage(),
+        );
+      },
     );
   }
 }
--- primeiro_projeto_flutter/lib/home_page.dart
+++ primeiro_projeto_flutter/lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:primeiro_projeto_flutter/app_controller.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,11 +12,93 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends State<HomePage> {
   int counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Center(child: Text('Home Page')),
+        actions: const [CustomSwitcher()],
+      ),
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: ListView(
+          children: [
+            Container(
+              height: 50.0,
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Text(
+                'Contador: $counter',
+              ),
+              const CustomSwitcher()
+            ]),
+            Container(
+              height: 50.0,
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Text(
+                'Contador: $counter',
+              ),
+              const CustomSwitcher()
+            ]),
+            Container(
+              height: 50.0,
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Text(
+                'Contador: $counter',
+              ),
+              const CustomSwitcher()
+            ]),
+            Container(
+              height: 50.0,
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Text(
+                'Contador: $counter',
+              ),
+              const CustomSwitcher()
+            ]),
+            Container(
+              height: 50.0,
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Text(
+                'Contador: $counter',
+              ),
+              const CustomSwitcher()
+            ]),
+            Container(
+              height: 50.0,
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Text(
+                'Contador: $counter',
+              ),
+              const CustomSwitcher()
+            ]),
+            Container(
+              height: 50.0,
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Text(
+                'Contador: $counter',
+              ),
+              const CustomSwitcher()
+            ]),
+            Container(
+              height: 50.0,
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Text(
+                'Contador: $counter',
+              ),
+              const CustomSwitcher()
+            ]),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
@@ -25,18 +108,26 @@ class HomePageState extends State<HomePage> {
           });
         },
       ),
-      body: Container(
-        height: 200,
-        width: 200,
-        color: Colors.black,
-        child: Center(
-          child: Container(
-            height: 100,
-            width: 100,
-            color: Colors.green,
-          ),
-        ),
-      ),
     );
+  }
+}
+
+class CustomSwitcher extends StatefulWidget {
+  const CustomSwitcher({super.key});
+
+  @override
+  State<CustomSwitcher> createState() => _CustomSwitcherState();
+}
+
+class _CustomSwitcherState extends State<CustomSwitcher> {
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+        value: AppController.instance.isDartTheme,
+        onChanged: (value) {
+          setState(() {
+            AppController.instance.changeTheme();
+          });
+        });
   }
 }
